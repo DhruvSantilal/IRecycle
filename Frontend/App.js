be0f582526004681aa82c0f7e55c520c9004850f
AseemAnsar <73474297+AseemAnsar@users.noreply.github.com>
2021-01-26 19:34:44 +0000

    Sign in Draft 2
    Please do not use this for now

---
+++ Frontend/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import './Form.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form classname='form'>
+     <h1>ENTER YOUR DETAILS TO REGISTER</h1>
+     <div class="form-label">
+       <label>Email: </label>
+       <input
+         
+         className='form-input'
+         type='email'
+         name='email'
+         placeholder='Enter your email'
+       />
+    </div>
+    <div class="form-label">
+       <label>Password: </label>
+       <input
+         
+         className='form-input'
+         type='password'
+         name='password'
+         placeholder='Enter your password'
+       />
+    </div>
+    <button className='form-input-btn' type='submit'>
+          Sign in
+        </button>
+        <span className='form-input-login'>
+          Not registered? Register <a href="#">here.</a>
+          {/* above is just a text asking for the users to login if they have an account */}
+        </span>
+   
+        </form>
+    </div>
+  );
+}
+
+export default App;
+
+
+//className='form-input'
+//type='email'
+//name='email'
+//placeholder='Enter your email'
