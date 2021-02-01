6fdaa9cb5f842752d925fabcb79b6177ccba5dfd
DhruvSantilal <73472888+DhruvSantilal@users.noreply.github.com>
2021-02-01 22:33:25 +0000

    Made a help section on footer

--- Frontend/IRecycle/app.js
+++ Frontend/IRecycle/app.js
@@ -1,8 +1,8 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
-tl.to(".slider", { y: "-100%", duration: 1, delay: 0.6 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=0.9");
+// tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
+// tl.to(".slider", { y: "-100%", duration: 1, delay: 0.6 });
+// tl.to(".intro", { y: "-100%", duration: 1 }, "-=0.9");
 tl.fromTo(".navbar", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".landing", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=1.2");
 // tl.fromTo(".search", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=2");
