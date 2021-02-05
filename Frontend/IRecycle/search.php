21bac3d8c0b9ce182cf8afcab31c7ef4dd212f91
esnyp <1915345@brunel.ac.uk>
2021-02-05 15:16:59 +0000

    Adjusted search bar size. Added footer

--- Frontend/IRecycle/search.php
+++ Frontend/IRecycle/search.php
@@ -41,6 +41,52 @@ include_once 'dbcon.php';
 
 
 
+
+
+
+
+<footer class="footer">
+                <div class="help">
+                    <div class="left-help">
+                        <div class="adress">
+                            <i class="far fa-address-card"></i>
+                            <h1>Adress</h1>
+                            <p>100 imaginaery <br>
+                                special avenue <br>
+                                RN9 10RB
+                            </p>
+                        </div>
+                        <div class="phone">
+                            <i class="fas fa-phone"></i>
+                            <h1>Number</h1>
+                            <p>0200 1212 1212</p>
+                        </div>
+                        <div class="email">
+                            <i class="fas fa-envelope-square"></i>
+                            <h1>Email</h1>
+                            <p>imaginaery@linkedin.com</p>
+                        </div>
+                    </div>
+                    <div class="right-help">
+                        <h1>How can we help?</h1>
+                        
+                        <label class="fullname-label" for="fullname">Full name</label>
+                        <input class="fullname-input" type="text" id="fullname" name="fullname" ><br>
+                        
+                        
+                        <label class="email-label" for="email">Email</label>
+                        <input class="email-input" type="text" id="email" name="email" ><br><br>
+                        
+                        
+
+                        <input class="empty" type="text" id="empty" name="empty" placeholder="Additional description"><br>
+
+                        <button class="button" type="button">Send</button>
+                    </div>
+                    
+                </div>
+                <h1 class="copyright">Copyright © by IRecycle</h1>
+            </footer>
 </body>
 </html>
 
