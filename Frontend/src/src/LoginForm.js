08527f8f783989e58183af6d44743333f5c0b916
DhruvSantilal <73472888+DhruvSantilal@users.noreply.github.com>
2021-02-11 17:50:24 +0000

    Linked profile page to home

---
+++ Frontend/src/src/LoginForm.js
@@ -0,0 +1,37 @@
+import react, {useState} from 'react'
+import './Form.css';
+
+function LoginForm({Login,error}) {
+    const [details, setDetails] = useState({name:"",email:"",password:""});
+const submitHandler = e => {
+    e.preventDefault();
+    Login(details);
+}
+
+return (
+    <form onSubmit={submitHandler}>
+    <div className="form-inner">
+        <h2>Login</h2>
+        {/*ERROR! */}
+        <div className="form-group">
+            <label htmlFor="username">Username:</label>
+           <input type="text" username="username" id="username" onChange={e =>setDetails({...details, username: e.target.value})} 
+           value = {details.username}/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" onChange={e =>setDetails({...details, email: e.target.value})} 
+           value = {details.email}/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" onChange={e =>setDetails({...details, password: e.target.value})} 
+           value = {details.password}/>
+        </div>
+        <input type="submit" value="LOGIN"/>
+    </div>
+    </form>
+)
+
+}
+export default LoginForm
