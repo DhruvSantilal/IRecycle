b090d26e275f8644cf58d4893a163c5db508919a
DhruvSantilal <73472888+DhruvSantilal@users.noreply.github.com>
2021-02-24 15:38:29 +0000

    added validation with js to footer form

--- Frontend/IRecycle/app.js
+++ Frontend/IRecycle/app.js
@@ -29,19 +29,49 @@ const empty = document.getElementById('empty')
 //variables
 
 const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
 
-form.addEventListener('button', (e) => {
+const errorElement = document.getElementById('error-fullname')
+const errorElement1 = document.getElementById('error-email')
+const errorElement2= document.getElementById('error-description')
+
+form.addEventListener('submit', (e) => {
     let messages = []
     if( fullname.value == null || fullname.value === '' ){
         messages.push('full name is required')
+        
     }
-    if(email.value.length == null || email.value.length == "" ){
-        messages.push('email is required')
+    if( fullname.value.length >= 40  ){
+        messages.push('full name should be less than 40');
+        
     }
     if(messages.length > 0){
         e.preventDefault();
-        errorElement.innerText = messages.join(', ')
+        errorElement.innerText = messages.join(',  ')
+    }
+    
+   
+})
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if(email.value.length == null || email.value.length == "" ){
+        messages.push('email is required')
+       
+    }
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement1.innerText = messages.join(',  ')
+    }
+   
+})
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if(empty.value.length == null || empty.value.length == "" ){
+        messages.push('description is required')
+       
+    }
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement2.innerText = messages.join(',  ')
     }
    
 })
