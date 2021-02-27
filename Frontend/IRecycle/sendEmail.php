b53f86f7e35290009264f76ae6736b8b50d1fa3a
esnyp <1915345@brunel.ac.uk>
2021-02-27 03:04:03 +0000

    Added alert style message to sucessful email message

--- Frontend/IRecycle/sendEmail.php
+++ Frontend/IRecycle/sendEmail.php
@@ -44,7 +44,7 @@ try {
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo "<script type='text/javascript'>alert('Message Sent');</script>";
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
