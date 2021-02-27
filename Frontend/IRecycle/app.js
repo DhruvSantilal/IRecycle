d613ee3d506c16bf241fc3899c0fe3c93cf04767
DhruvSantilal <73472888+DhruvSantilal@users.noreply.github.com>
2021-02-27 23:58:59 +0000

    fixed footer form validation being deleted

--- Frontend/IRecycle/app.js
+++ Frontend/IRecycle/app.js
@@ -22,7 +22,7 @@ tl.fromTo(".footer", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=3");
 tl.fromTo(".search-results", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=3");
 tl.fromTo(".results-container", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=2.5");
 
-//form valodation
+//form valodation footer
 const fullname = document.getElementById('fullname');
 const email = document.getElementById('email');
 const empty = document.getElementById('empty')
@@ -75,3 +75,66 @@ form.addEventListener('submit', (e) => {
     }
    
 })
+
+
+
+
+
+// form validation post an ad
+
+
+// const category = document.getElementById('category');
+// const location = document.getElementById('location');
+// const postTitle = document.getElementById('postTitle');
+// const postImage = document.getElementById('postImage');
+// const adDescription = document.getElementById('adDescription');
+
+// //variables
+
+// // const formPost = document.getElementById('formPost')
+
+// const errorElement3 = document.getElementById('error-category')
+// // const errorElement1 = document.getElementById('error-email')
+// // const errorElement2= document.getElementById('error-description')
+
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if( category.value == null || category.value === '' ){
+//         messages.push('full name is required')
+        
+//     }
+//     if( category.value.length >= 40  ){
+//         messages.push('full name should be less than 40');
+        
+//     }
+//     if(category.length > 0){
+//         e.preventDefault();
+//         errorElement.innerText = messages.join(',  ')
+//     }
+    
+   
+// })
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if(email.value.length == null || email.value.length == "" ){
+//         messages.push('email is required')
+       
+//     }
+//     if(messages.length > 0){
+//         e.preventDefault();
+//         errorElement1.innerText = messages.join(',  ')
+//     }
+   
+// })
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if(empty.value.length == null || empty.value.length == "" ){
+//         messages.push('description is required')
+       
+//     }
+//     if(messages.length > 0){
+//         e.preventDefault();
+//         errorElement2.innerText = messages.join(',  ')
+//     }
+   
+// })
