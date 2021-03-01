9523815f497da10bc9c500a36d168d34e40a9995
esnyp <1915345@brunel.ac.uk>
2021-03-01 08:04:28 +0000

    Added footer file, will implement it to all pages soon.

--- Frontend/IRecycle/search.php
+++ Frontend/IRecycle/search.php
@@ -34,53 +34,12 @@ include 'nav.php';
 
 
 
+<?php
 
-<footer class="footer">
-                <div class="help">
-                    <div class="left-help">
-                        <div class="address">
-                            <i class="far fa-address-card"></i>
-                            <h1>Address</h1>
-                            <p>100 Placeholder Avenue <br>                           
-                                RN9 10RB
-                            </p>
-                        </div>
-                        <div class="phone">
-                            <i class="fas fa-phone"></i>
-                            <h1>Number</h1>
-                            <p>0200 1212 1212</p>
-                        </div>
-                        <div class="email">
-                            <i class="fas fa-envelope-square"></i>
-                            <h1>Email</h1>
-                            <p>irecycleldn@gmail.com</p>
-                        </div>
-                    </div>
-
-                    <div class="right-help">
-                      <form class="contact-form" action="" method="post">
-
-                    <legend><h3>Contact Us</legend><br>
+include 'footer.php';
+?>
 
 
-                      <label for="name">Name</label>   
-                      <input type="text" name="name" maxlength="30" required><br>
-
-                      <label for="email">E-mail</label> 
-                      <input type="email" name="email" maxlength="75" required ><br>
-
-                      <label for="subject">Subject</label> 
-                      <input type="text" name="subject" maxlength="25" required><br>
-
-                      <label for="message">Your message</label> 
-                      <textarea name="message" maxlength="1000" placeholder = "Max length 1000" required></textarea><br>
-
-                      <button type="submit" name= "submit">Send Mail</button>
-                      </form>
-
-                </div>
-                <h1 class="copyright">Copyright © by IRecycle</h1>
-            </footer>
 </body>
 </html>
 
