8d30a32571a9727c1e954bfb3eea86fd8a6a0d8a
esnyp <1915345@brunel.ac.uk>
2021-03-06 21:38:19 +0000

    Converted about and login page to php. Added footer and nav bar to index page. Fixed css errors.

--- Frontend/IRecycle/searchResults.php
+++ Frontend/IRecycle/searchResults.php
@@ -3,7 +3,7 @@ include_once 'dbcon.php';
 ?>
 
 <head>
-  <title>Document</title>
+  <title>".$queryResult." results!" "Found</title>
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./style.css" />
